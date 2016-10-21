9bde5c502df073d3fe9c2b30bc0ab20bc6cb8f71
Justin Hines <jhines@digitalocean.com>
2016-10-21 13:45:34 -0400

    giphy

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ MAINTAINER Justin Hines "justinphines@gmail.com"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-EXPOSE 5000
+EXPOSE 80
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect
+import giphypop
 
 app = Flask(__name__)
 
+g = giphypop.Giphy()
+
 @app.route("/")
 def hello():
-    return "I'm a one click lover!"
-
+    return redirect(g.random_gif().media_url, code=302)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=80)
