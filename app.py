3fa4c53e05a26caf1c9e64dbd3d63c114ed6da7c
Justin Hines <jhines@digitalocean.com>
2016-10-21 13:59:12 -0400

    cornify

--- app.py
+++ app.py
@@ -7,7 +7,17 @@ g = giphypop.Giphy()
 
 @app.route("/")
 def hello():
-    return redirect(g.random_gif().media_url, code=302)
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
+</head>
+<body onload="javascript:cornify_add()">>
+<img src="%s">
+</body>
+</html>
+""" % g.random_gif().media_url
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=80)
